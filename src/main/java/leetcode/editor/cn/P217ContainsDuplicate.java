6c92074cf7f7746f635e8410ac9b3533165ce607
哈哈菌 <harrisonli60@163.com>
2019-11-20 11:34:59 +0800

    @dafeigefeifeifei, P217 AC

---
+++ src/main/java/leetcode/editor/cn/P217ContainsDuplicate.java
@@ -0,0 +1,75 @@
+//给定一个整数数组，判断是否存在重复元素。 
+//
+// 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。 
+//
+// 示例 1: 
+//
+// 输入: [1,2,3,1]
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入: [1,2,3,4]
+//输出: false 
+//
+// 示例 3: 
+//
+// 输入: [1,1,1,3,3,4,3,2,4,2]
+//输出: true 
+// Related Topics 数组 哈希表
+
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+//Java：存在重复元素
+public class P217ContainsDuplicate {
+
+	@Test
+	public void testResult() {
+		Solution solution = new P217ContainsDuplicate().new Solution();
+		// TO TEST
+		int[] nums = {1, 2, 3, 1};
+		boolean ret = solution.containsDuplicate(nums);
+		Assert.assertEquals(true, ret);
+	}
+
+	@Test
+	public void testResult2() {
+		Solution solution = new P217ContainsDuplicate().new Solution();
+		// TO TEST
+		int[] nums = {1, 2, 3, 4};
+		boolean ret = solution.containsDuplicate(nums);
+		Assert.assertEquals(false, ret);
+	}
+
+	@Test
+	public void testResult3() {
+		Solution solution = new P217ContainsDuplicate().new Solution();
+		// TO TEST
+		int[] nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+		boolean ret = solution.containsDuplicate(nums);
+		Assert.assertEquals(true, ret);
+	}
+
+	//leetcode submit region begin(Prohibit modification and deletion)
+	class Solution {
+		public boolean containsDuplicate(int[] nums) {
+			Map<Integer, Integer> map = new HashMap<>();
+			for (int i = 0; i < nums.length; ++i) {
+				if (map.containsKey(nums[i])) {
+					return true;
+				} else {
+					map.put(nums[i], 1);
+				}
+			}
+			return false;
+		}
+	}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
