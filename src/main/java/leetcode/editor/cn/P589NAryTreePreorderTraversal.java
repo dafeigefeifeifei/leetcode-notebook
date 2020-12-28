28b8504ef0894441d36aa049233a5c2c00bf67fb
lh <harrisonli60@163.com>
2020-12-28 11:28:18 +0800

    P589 AC

--- src/main/java/leetcode/editor/cn/P589NAryTreePreorderTraversal.java
+++ src/main/java/leetcode/editor/cn/P589NAryTreePreorderTraversal.java
@@ -32,22 +32,22 @@ public class P589NAryTreePreorderTraversal {
 	//leetcode submit region begin(Prohibit modification and deletion)
 
 	// Definition for a Node.
-//	class Node {
-//		public int val;
-//		public List<Node> children;
-//
-//		public Node() {
-//		}
-//
-//		public Node(int _val) {
-//			val = _val;
-//		}
-//
-//		public Node(int _val, List<Node> _children) {
-//			val = _val;
-//			children = _children;
-//		}
-//	}
+	class Node {
+		public int val;
+		public List<Node> children;
+
+		public Node() {
+		}
+
+		public Node(int _val) {
+			val = _val;
+		}
+
+		public Node(int _val, List<Node> _children) {
+			val = _val;
+			children = _children;
+		}
+	}
 
 	class Solution {
 		public List<Integer> preorder(Node root) {
